cf66b589757482782e00a43246b5a400383dbd59
DorottyaKrisztics <dorka713@gmail.com>
2017-09-21 22:29:41 +0200

    Added data for demo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -101,15 +101,21 @@ public class Main {
         supplierDataStore.add(lenovo);
         Supplier Porn = new Supplier("Porn", "XXX");
         supplierDataStore.add(Porn);
+        Supplier eBay = new Supplier("eBay", "The biggest online store ever");
+        supplierDataStore.add(eBay);
+        Supplier ikea = new Supplier("IKEA", "Interior design");
+        supplierDataStore.add(ikea);
 
 
         //setting up a new product category
         ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to " +
                 "tablet, is a thin, flat mobile computer with a touchscreen display.");
         productCategoryDataStore.add(tablet);
-        ProductCategory Adult = new ProductCategory("Adult", "Hardware", "A XXX computer, commonly shortened  is a " +
-                "thin,  a touchscreen display.");
+        ProductCategory ball = new ProductCategory("Ball", "Toys", "A stuffed soft ball for playing in the class room");
+        productCategoryDataStore.add(ball);
+        ProductCategory Adult = new ProductCategory("Adult", "Hardware", "XXX XXX XXX");
         productCategoryDataStore.add(Adult);
+
 
         //setting up products and printing it
         productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good" +
@@ -118,8 +124,18 @@ public class Main {
                 "Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
         productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great " +
                 "value for media consumption.", tablet, amazon));
-        productDataStore.add(new Product("Anal intruder X", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great " +
-                "value for media consumption.", Adult, Porn));
+
+        productDataStore.add(new Product("Anal intruder X", 89, "USD","XXX XXX XXX", Adult, Porn));
+        productDataStore.add(new Product("LEKA Soft toy, ball, multicolor", 1.99f, "USD", "Fabric: 100 % polyester." +
+                "Filling: Polyester fiber fill. Recommended for all ages.", ball, ikea));
+        productDataStore.add(new Product("Pokemon ball", 12, "USD", "Gotta catch ‘em all! If you want to catch a Bulbasaur," +
+                "Weedle, Rattata or Jigglypuff Pokemon you’ll need a Pokeball, and this officially licensed Poke Ball Plush is ideal for any Pokemen trainers!", ball, eBay));
+        productDataStore.add(new Product("Yoda Soft Ball", 8.5f, "USD", "Star Wars fans will love these " +
+                "fantastic talking plush toys! Choose from Yoda, R2D2, Darth Vader and Chewbacca characters. ", ball, eBay));
+        productDataStore.add(new Product("Ty Beanie Ballz Fable Unicorn Plush", 16.99f, "USD", "\n" +
+                "Beanie Ballz are a little wild and whacky, toss 'em and they always land on their feet." +
+                " Collect them all.", ball, amazon));
+
 
         ShoppingCart.getInstance().addItem(1);
         ShoppingCart.getInstance().addItem(2);
