f733a8ee3d61b4fa10e41d8b1bb3e0f66b7bba75
Andras Szabo <andras.l.szabo@gmail.com>
2017-10-05 13:39:32 +0200

    test dir and test files created

---
+++ test/com/codecool/shop/dao/ProductCategoryDaoTest.java
@@ -0,0 +1,24 @@
+package com.codecool.shop.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductCategoryDaoTest {
+    @Test
+    void add() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+}
