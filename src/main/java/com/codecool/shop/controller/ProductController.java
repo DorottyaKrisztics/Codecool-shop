2fa52d109af5b51e900e50e7b341b88a270208ff
Peter Korossy <meanwhile@vipmail.hu>
2017-10-05 17:51:53 +0200

    Refactored project_controller

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -17,52 +17,38 @@ import java.util.Map;
 
 public class ProductController {
 
+    static ProductDao productDataStore = ProductDaoJdbc.getInstance();
+    static ProductCategoryDao productCategoryDataStore = ProductCategoryDaoJdbc.getInstance();
+    static SupplierDao supplierDataStore = SupplierDaoJdbc.getInstance();
+
     public static ModelAndView renderProducts(Request req, Response res) {
-        ProductDao productDataStore = ProductDaoMem.getInstance();
-        ProductDao productDataStore2 = ProductDaoJdbc.getInstance();
-        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
-        ProductCategoryDao productCategoryDataStore2 = ProductCategoryDaoJdbc.getInstance();
-        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
-        SupplierDao supplierDataStore2 = SupplierDaoJdbc.getInstance();
 
         Map params = new HashMap<>();
-        params.put("category", productCategoryDataStore2.getAll());
-        params.put("products", productDataStore2.getAll());
-        params.put("supplier", supplierDataStore2.getAll());
+        params.put("category", productCategoryDataStore.getAll());
+        params.put("products", productDataStore.getAll());
+        params.put("supplier", supplierDataStore.getAll());
         params.put("totalitemcount", ShoppingCart.getInstance().getTotalItemCount());
         return new ModelAndView(params, "product/index");
     }
 
     public static ModelAndView renderProductCategory(Request req, Response res, int id) {
-        ProductDao productDataStore = ProductDaoMem.getInstance();
-        ProductDao productDataStore2 = ProductDaoJdbc.getInstance();
-        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
-        ProductCategoryDao productCategoryDataStore2 = ProductCategoryDaoJdbc.getInstance();
-        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
-        SupplierDao supplierDataStore2 = SupplierDaoJdbc.getInstance();
 
         Map params = new HashMap<>();
-        params.put("allcategory", productCategoryDataStore2.getAll());
-        params.put("allsupplier", supplierDataStore2.getAll());
-        params.put("category", productCategoryDataStore2.find(id));
-        params.put("products", productDataStore2.getBy(productCategoryDataStore2.find(id)));
+        params.put("allcategory", productCategoryDataStore.getAll());
+        params.put("allsupplier", supplierDataStore.getAll());
+        params.put("category", productCategoryDataStore.find(id));
+        params.put("products", productDataStore.getBy(productCategoryDataStore.find(id)));
         return new ModelAndView(params, "product/category");
 
     }
 
     public static ModelAndView renderSupplier(Request req, Response res, int id) {
-        ProductDao productDataStore = ProductDaoMem.getInstance();
-        ProductDao productDataStore2 = ProductDaoJdbc.getInstance();
-        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
-        ProductCategoryDao productCategoryDataStore2 = ProductCategoryDaoJdbc.getInstance();
-        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
-        SupplierDao supplierDataStore2 = SupplierDaoJdbc.getInstance();
         Map params = new HashMap<>();
 
-        params.put("allsupplier", supplierDataStore2.getAll());
-        params.put("supplier", supplierDataStore2.find(id));
-        params.put("category", productCategoryDataStore2.getAll());
-        params.put("products", productDataStore2.getBy(supplierDataStore2.find(id)));
+        params.put("allsupplier", supplierDataStore.getAll());
+        params.put("supplier", supplierDataStore.find(id));
+        params.put("category", productCategoryDataStore.getAll());
+        params.put("products", productDataStore.getBy(supplierDataStore.find(id)));
         return new ModelAndView(params, "product/supplier");
 
     }
